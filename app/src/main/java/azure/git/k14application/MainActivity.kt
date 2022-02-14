805317b719bc7136d91b751767eaf82f4dad2125
Michal Lojewski <michal.lojewski@knowit.no>
2022-02-14 17:08:10 +0100

    added DB and button intergration

--- app/src/main/java/azure/git/k14application/MainActivity.kt
+++ app/src/main/java/azure/git/k14application/MainActivity.kt
@@ -2,10 +2,25 @@ package azure.git.k14application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    //
+    private lateinit var qrCodeImg : ImageView
+    private lateinit var outputData: TextView
+    private lateinit var btnGenerateQR: Button
+    private lateinit var scanQrButton: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnGenerateQR.findViewById<Button>(R.id.generateQR)
+        scanQrButton.findViewById<Button>(R.id.scanQr)
+        outputData.findViewById<TextView>(R.id.outputQR)
     }
 }
